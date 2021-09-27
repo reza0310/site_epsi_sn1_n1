63e3f2188551b960bed9f2394f8b105d13942795
reza0310 <70545529+reza0310@users.noreply.github.com>
2021-09-27 11:20:15 +0200

    Add files via upload

---
+++ dbmaker part 2.php
@@ -0,0 +1,32 @@
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "Site1Database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE TABLE tickets(
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+salle VARCHAR(5) NOT NULL,
+nom VARCHAR(50) NOT NULL,
+prenom VARCHAR(50) NOT NULL,
+email VARCHAR(50),
+sujet VARCHAR(30) NOT NULL,
+type enum('EAU','GAZ','PROJECTEUR'),
+description VARCHAR(500)
+)";
+if ($conn->query($sql) === TRUE) {
+  echo "Success";
+} else {
+  echo "Error" . $conn->error;
+}
+
+$conn->close();
+?> 
