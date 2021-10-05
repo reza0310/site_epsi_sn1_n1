3033dcaa8601acd7ad0f8bf76aacdb349a07052c
reza0310 <70545529+reza0310@users.noreply.github.com>
2021-10-05 09:46:26 +0200

    Add files via upload

---
+++ ouverture_ticket/confirmation/index.php
@@ -0,0 +1,40 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$db = "site1database";
+
+$salle = $_POST["salle"];
+$nom = $_POST["nom"];
+$prenom = $_POST["prenom"];
+$email = $_POST["email"];
+$sujet = $_POST["sujet"];
+$type = $_POST["type"];
+$description = $_POST["description"];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO tickets (salle, nom, prenom, email, sujet, type, description) VALUES (\"".$salle."\", 
+\"".$nom."\",
+\"".$prenom."\",
+\"".$email."\",
+\"".$sujet."\",
+\"".$type."\",
+\"".$description."\")";
+
+echo ($sql);
+
+if ($conn->query($sql) === TRUE) {
+	set_include_path($_SERVER['DOCUMENT_ROOT']."projet_site");
+	echo(str_replace("%php%", file_get_contents("page.html"), file_get_contents("header.html", true)));
+} else {
+	echo "Request error: " . $conn->error;
+}
+
+$conn->close();
+?>
