2f00c334a7190bf3b56d9204f6008c97729a22cd
reza0310 <70545529+reza0310@users.noreply.github.com>
2021-10-05 11:41:05 +0200

    Add files via upload

--- connexion/problemes/index.php
+++ connexion/problemes/index.php
@@ -1,4 +1,37 @@
 <?php
 set_include_path($_SERVER['DOCUMENT_ROOT']."projet_site");
-echo(str_replace("%php%", file_get_contents("page.html"), file_get_contents("header.html", true)));
+$pseudo = $_POST["pseudo"];
+$mdp = $_POST["password"];
+if ($pseudo == "Technicien" && $mdp == "324JGI") {
+	
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+	$db = "site1database";
+	
+	$con=mysqli_connect($servername,$username,$password,$db);
+	if (mysqli_connect_errno()) {
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	  exit();
+	}
+	$sql = "SELECT * FROM tickets";
+	$result = mysqli_query($con,$sql);
+	
+	echo ("Bienvenue technicien!<br>"); 
+	$page = "";
+	while ($colonne = mysqli_fetch_array($result)) {
+		$page .= "<div class='ticket'>";
+		$page .= "Demandeur: ".$colonne[3]." ".$colonne[2]."<br>";
+		$page .= "Mail: ".$colonne[4]."<br>";
+		$page .= "Salle: ".$colonne[1]."<br>";
+		$page .= "Problème de: ".$colonne[6]."<br>";
+		$page .= "Intitulé: ".$colonne[5]."<br>";
+		$page .= "Problème développé: ".$colonne[7]."<br>";
+		$page .= "</div>";
+	}
+	echo(str_replace("%php%", $page, file_get_contents("header.html", true)));
+	mysqli_close($con);
+} else {
+	header( "Location: /projet_site" );
+}
 ?>
